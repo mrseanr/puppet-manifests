bfde2b4a039bf01a5b5e863fad3125a970231f2b
Sean Rasmussen <sean@thecomputerconnectioninc.com>
2016-03-03 21:43:28 -0600

    added zookeeper tarball, extraction

--- zookeeper/site.pp
+++ zookeeper/site.pp
@@ -6,7 +6,7 @@ $zk3f = "${zkf}/zk3"
 
 # notify { "Zookeeper folder ${zk3f}": }
 
-file{ [ "${esf}",
+file { [ "${esf}",
 	"${zkf}",
 	"${zk1f}",
 	"${zk2f}",
@@ -16,12 +16,23 @@ file{ [ "${esf}",
 	"${zk3f}/zkdata" ] :
 	ensure => directory,
 }
-file{ "${zk1f}/zkdata/myid":
+file { "${zk1f}/zkdata/myid":
 	content => '1',
 }
-file{ "${zk2f}/zkdata/myid":
+file { "${zk2f}/zkdata/myid":
 	content => '2',
 }
-file{ "${zk3f}/zkdata/myid":
+file { "${zk3f}/zkdata/myid":
 	content => '3',
+}
+file { "${zkf}/zookeeper-3.4.6.tar.gz":
+	ensure => present,
+	mode => 0666,
+	source => '/etc/puppet/environments/production/manifests/resources/zookeeper-3.4.6.tar.gz',
+	before => Exec['unpack_file_zookeeper.tar.gz'],
+}
+exec {'unpack_file_zookeeper.tar.gz':
+	unless => 'test -f /usr/tmp/ent_search/zookeeper/zk1/bin/zkserver.sh',
+	cwd => "${zkf}",
+	command => 'tar xvfz zookeeper-3.4.6.tar.gz'
 }
