c8d61a6958e59ccb3c6e642b380b1e1cb5312528
Sean Rasmussen <mr.sean.r@gmail.com>
2016-03-04 20:25:27 -0600

    Added comments

--- zookeeper/site.pp
+++ zookeeper/site.pp
@@ -1,3 +1,4 @@
+# This script will setup multiple zookeepers on a single server.
 $esf = "/usr/tmp/ent_search"
 $zkf = "${esf}/zookeeper"
 $zk1f = "${zkf}/zk1"
@@ -16,6 +17,7 @@ file { [ "${esf}",
 	"${zk3f}/zkdata" ] :
 	ensure => directory,
 }
+# Build the myid file required for zookeepers
 file { "${zk1f}/zkdata/myid":
 	content => '1',
 }
@@ -25,14 +27,17 @@ file { "${zk2f}/zkdata/myid":
 file { "${zk3f}/zkdata/myid":
 	content => '3',
 }
+# Move the zookeeper-3.4.6.tar.gz to the zk main folder (FOR TESTING maybe used for prod if a download isn't possible.)
 file { "${zkf}/zookeeper-3.4.6.tar.gz":
 	ensure => present,
 	mode => 0666,
 	source => '/etc/puppet/environments/production/manifests/resources/zookeeper-3.4.6.tar.gz',
 	before => Exec['unpack_file_zookeeper.tar.gz'],
 }
+# Use the -C (capital c) with a folder path to output to a specific folder.
+# This needs repeating (for building multiple zookeepers on a single box)
 exec {'unpack_file_zookeeper.tar.gz':
 	unless => 'test -f /usr/tmp/ent_search/zookeeper/zk1/bin/zkserver.sh',
 	cwd => "${zkf}",
-	command => 'tar xvfz zookeeper-3.4.6.tar.gz'
+	command => "tar xvfz zookeeper-3.4.6.tar.gz -C zk1"
 }
